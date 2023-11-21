812ad8ad7661820ff67312269a6de2f945c24d3e
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 21:21:22 -0800

    will it work? dnt know

--- mirror.sh
+++ mirror.sh
@@ -4,9 +4,8 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
-git remote add codecommit codecommit::us-east-2://ldcore-demo-1
-git remote set-url codecommit "$INPUT_TARGET_REPO_URL"
-push -u codecommit codecommit-demo-1:main --force
+git remote add codecommit "$INPUT_TARGET_REPO_URL"
+git push codecommit codecommit-demo-1:main --force
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
 # run the command by hand.
