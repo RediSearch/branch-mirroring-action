a63963c258e04151f798a179cb32168c8e6d4423
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 21:26:02 -0800

    logging

--- mirror.sh
+++ mirror.sh
@@ -4,7 +4,9 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+cat /github/file_commands
 git remote add origin2 "$INPUT_TARGET_REPO_URL"
+
 git push origin2 codecommit-demo-1:main --force
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
