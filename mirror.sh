7718b20fd0ae37511956412bdb70e89ac1e43cd0
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 20:38:45 -0800

    fixing remote add again

--- mirror.sh
+++ mirror.sh
@@ -6,6 +6,8 @@ set -eu
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 # git remote add -m "$INPUT_TARGET_REPO_URL"
 git remote set-url origin "$INPUT_TARGET_REPO_URL"
+git config --local user.name actions-user
+git config --local user.email "actions@github.com"
 git commit -m "initial commit"
 git push -u origin main --force
 
