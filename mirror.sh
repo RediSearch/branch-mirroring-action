ebbcef8c9ce1d440a29e0e7580c2d48e4839b59e
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 20:54:17 -0800

    setting new branch

--- mirror.sh
+++ mirror.sh
@@ -4,6 +4,7 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+git checkout codecommit-demo-1
 # git remote add -m "$INPUT_TARGET_REPO_URL"
 git remote set-url origin "$INPUT_TARGET_REPO_URL"
 git config --local user.name actions-user
