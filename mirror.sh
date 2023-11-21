2866c66108e30771c4530059dcd69ff3542f2cd5
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 21:29:03 -0800

    add all

--- mirror.sh
+++ mirror.sh
@@ -4,10 +4,9 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
-cat /github/file_commands
-git remote add origin2 "$INPUT_TARGET_REPO_URL"
+git remote add origin3 ssh://git-codecommit.us-east-2.amazonaws.com/v1/repos/ldcore-demo-1
 
-git push origin2 codecommit-demo-1:main --force
+git push origin3 codecommit-demo-1:main --force
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
 # run the command by hand.
