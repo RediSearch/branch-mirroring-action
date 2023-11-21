12f635d953b17882551acbf373ac2ba0a6c018aa
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 21:10:21 -0800

    fixing

--- mirror.sh
+++ mirror.sh
@@ -4,6 +4,7 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+git remote add codecommit codecommit::us-east-2://ldcore-demo-1
 git remote set-url codecommit "$INPUT_TARGET_REPO_URL"
 git config --local user.name actions-user
 git config --local user.email "actions@github.com"
