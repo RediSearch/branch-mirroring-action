1d301da12a178146cf325760989bcd08acad95ec
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 21:13:35 -0800

    fixing

--- mirror.sh
+++ mirror.sh
@@ -6,9 +6,6 @@ set -eu
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add codecommit codecommit::us-east-2://ldcore-demo-1
 git remote set-url codecommit "$INPUT_TARGET_REPO_URL"
-git config --local user.name actions-user
-git config --local user.email "actions@github.com"
-git commit -m "initial commit"
 push -u codecommit codecommit-demo-1:main --force
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
