fe881591bb3f02c0329fd65acba5b35defe9d0d7
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 23:01:39 -0800

    adding force

--- mirror.sh
+++ mirror.sh
@@ -6,7 +6,7 @@ set -eu
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add upstream "$INPUT_TARGET_REPO_URL"
 # git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
-git push -u upstream $INPUT_SOURCE_BRANCH_ID:main
+git push -u upstream $INPUT_SOURCE_BRANCH_ID:main -f
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
 # run the command by hand.
 /cleanup.sh mirror
