7104518710213a68405e25b54bdbc470895206f1
Cody De Arkland <codydearkland@gmail.com>
2023-11-20 22:24:40 -0800

    back to beginning

--- mirror.sh
+++ mirror.sh
@@ -4,9 +4,8 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
-git remote add origin3 ssh://git-codecommit.us-east-2.amazonaws.com/v1/repos/ldcore-demo-1
-
-git push origin3 codecommit-demo-1:main --force
+git remote add mirror "$INPUT_TARGET_REPO_URL"
+git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
 # run the command by hand.
